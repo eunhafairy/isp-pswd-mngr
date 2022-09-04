fa46ce59ae113f7945f74a887300a03b9ff37d1f
iver <ipesiganifc@gmail.com>
2022-09-04 11:40:42 +0800

    commented express

--- src/index.js
+++ src/index.js
@@ -7,28 +7,18 @@ import Login from './pages/login/Login';
 import Home from './pages/home/Home';
 import Register from './pages/register/register';
 import { BrowserRouter, Routes,  Route } from "react-router-dom";
-// express imports
-const express = require('express'),
-http = require('http');
+// // express imports
+// const express = require('express'),
+// http = require('http');
 
 
 // ------ code -------
 // express config
-const hostname = 'localhost';
-const port = 8080;
-const app = express();
-app.use((req, res) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is a test server</h1></body></html>');
- 
-});
-const sample_server = http.createServer(app);
- 
-sample_server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-})
+// const hostname = 'localhost';
+// const port = 8080;
+// const app = express();
+
+
 
 // react render
 const root = ReactDOM.createRoot(document.getElementById('root'));
